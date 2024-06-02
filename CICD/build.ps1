32947d0267217ece05e389c3a5ff53137fe42fc7
PabloVillapunMartin <pvillapunmartin@gmail.com>
2024-06-02 20:15:19 +0200

    Revert CICD changes

--- CICD/build.ps1
+++
@@ -1,5 +0,0 @@
-# Build script for Windows
-$UE4Path = "C:\Program Files\Epic Games\UE_5.0"
-$ProjectPath = "${{ github.workspace }}\YourProject\YourProject.uproject"
-
-& "$UE4Path\Engine\Build\BatchFiles\Build.bat" YourProjectEditor Win64 Development -project="$ProjectPath" -waitmutex -nocompileeditor -noclean
