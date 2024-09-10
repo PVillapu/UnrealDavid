421dffbef4cfa5c111b558f2f68292c938ff8028
PVillapu <140111335+PVillapu@users.noreply.github.com>
2024-09-10 18:42:00 +0200

    Added Game event log declaration

--- David/Source/David/Gameplay/Misc/CustomDavidLogs.h
+++ David/Source/David/Gameplay/Misc/CustomDavidLogs.h
@@ -1,3 +1,5 @@
 #include "CoreMinimal.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogDavid, Display, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogDavid, Log, All);
+
+DECLARE_LOG_CATEGORY_EXTERN(LogDavidGameEvent, Log, All);
